9a418d1aefe0b169629bea4158c155115ff77209
Kyle <kbburkholder@gmail.com>
2024-07-31 12:55:09 -0400

    adding debugging consoles

--- app.js
+++ app.js
@@ -12,5 +12,8 @@ const sync_tracks_without_overlap = async () => {
 
 let isPreviousSyncRunning = false
 let sync_interval = 60000
+console.log("outputting env vars to ensure proper settings")
+console.log(`supabase_url: ${process.env.supabase_url}`)
+console.log(`last_fm_user: ${process.env.last_fm_user}`)
 console.log(`Syncing every ${sync_interval / 1000} seconds starting in ${sync_interval / 1000} seconds`)
 setInterval(sync_tracks_without_overlap, 60000)
